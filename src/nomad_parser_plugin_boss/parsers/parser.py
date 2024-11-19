6744cd22f134a23f120f1d71d153f9f614c8f43c
ndaelman <ndaelman@physik.hu-berlin.de>
2024-11-19 12:43:30 +0100

    Add first draft

--- src/nomad_parser_plugin_boss/parsers/parser.py
+++ src/nomad_parser_plugin_boss/parsers/parser.py
@@ -11,15 +11,37 @@ if TYPE_CHECKING:
     )
 
 from nomad.config import config
-from nomad.datamodel.metainfo.workflow import Workflow
 from nomad.parsing.parser import MatchingParser
+from nomad.parsing.file_parser.text_parser import TextParser, Quantity as TextQuantity
+
+from nomad_parser_plugin_boss.schema_packages.schema_package import PotentialEnergySurfaceFit
 
 configuration = config.get_plugin_entry_point(
     'nomad_parser_plugin_boss.parsers:parser_entry_point'
 )
 
 
-class NewParser(MatchingParser):
+class BossSliceParser(TextParser):
+    def __init__(self):
+        super().__init__(None)
+
+    def init_quantities(self):
+        def split_row(full_row: str, key_defs: tuple[str] = ('x_1', 'x_2', 'mu', 'nu')) -> dict[str, float]:
+            return {k: float(x) for k, x in zip(key_defs, full_row.split())}
+
+        # re_float = r'\d\.\d+e[\-\+]\d{2}'
+
+        table_quantities = [
+            TextQuantity(
+                'row',
+                r'((?:\s+\d\.\d+e[\-\+]\d{2}){4})\n',
+                str_operation=split_row,
+                repeats=True,
+            ),
+        ]
+
+
+class BossPostProcessingParser(MatchingParser):
     def parse(
         self,
         mainfile: str,
@@ -27,6 +49,19 @@ class NewParser(MatchingParser):
         logger: 'BoundLogger',
         child_archives: dict[str, 'EntryArchive'] = None,
     ) -> None:
-        logger.info('NewParser.parse', parameter=configuration.parameter)
+        logger.info('BossPostProcessingParser.parse', parameter=configuration.parameter)
 
-        archive.workflow2 = Workflow(name='test')
+        slice_parser = BossSliceParser(mainfile=mainfile, logger=logger)
+        slice_parser.parse()
+
+        archive.data = [
+            PotentialEnergySurfaceFit(
+                parameter_1_name='parameter_1_name',
+                parameter_1_values=set(slice_parser.get_quantity('row').data[0]['x_1']),
+                parameter_2_name='parameter_2_name',
+                parameter_2_values=set(slice_parser.get_quantity('row').data[0]['x_2']),
+                energy_values=slice_parser.get_quantity('row').data[0]['mu'],
+                energy_variance=slice_parser.get_quantity('row').data[0]['nu'],
+            )
+        ]
+        
--- src/nomad_parser_plugin_boss/schema_packages/schema_package.py
+++ src/nomad_parser_plugin_boss/schema_packages/schema_package.py
@@ -10,9 +10,12 @@ if TYPE_CHECKING:
         BoundLogger,
     )
 
+import numpy as np
+
 from nomad.config import config
 from nomad.datamodel.data import Schema
 from nomad.datamodel.metainfo.annotations import ELNAnnotation, ELNComponentEnum
+from nomad.datamodel.metainfo.plot import PlotSection, PlotlyFigure
 from nomad.metainfo import Quantity, SchemaPackage
 
 configuration = config.get_plugin_entry_point(
@@ -22,17 +25,68 @@ configuration = config.get_plugin_entry_point(
 m_package = SchemaPackage()
 
 
-class NewSchemaPackage(Schema):
-    name = Quantity(
-        type=str, a_eln=ELNAnnotation(component=ELNComponentEnum.StringEditQuantity)
+class PotentialEnergySurfaceFit(PlotSection, Schema):
+    # m_def = Section()
+
+    parameter_1_name = Quantity(
+        type=str,
+        a_eln=ELNAnnotation(component=ELNComponentEnum.StringEditQuantity),
     )
-    message = Quantity(type=str)
+
+    parameter_1_values = Quantity(
+        type=np.float64,
+        shape=['*'],
+    )  # ! TODO use `PhysicalProperty`
+    
+    parameter_2_name = Quantity(
+        type=str,
+        a_eln=ELNAnnotation(component=ELNComponentEnum.StringEditQuantity),
+    )
+
+    parameter_2_values = Quantity(
+        type=np.float64,
+        shape=['*'],
+    )  # ! TODO use `PhysicalProperty`
+
+    energy_values = Quantity(
+        type=np.float64,
+        unit='eV',  # ?
+        shape=['*', '*'],
+    )  # ! TODO use `PhysicalProperty`
+
+    energy_variance = Quantity(
+        type=np.float64,
+        unit='eV^2',  # ?
+        shape=['*', '*'],
+    )  # ! TODO use `PhysicalProperty`
 
     def normalize(self, archive: 'EntryArchive', logger: 'BoundLogger') -> None:
-        super().normalize(archive, logger)
+        super(Schema, self).normalize(archive, logger)
 
-        logger.info('NewSchema.normalize', parameter=configuration.parameter)
-        self.message = f'Hello {self.name}!'
+        try:
+            if self.energy_values.shape != (len(self.parameter_1_values), len(self.parameter_2_values)):
+                raise ValueError('Energy values shape does not match parameter values')  # ?
+
+            self.figures.append(
+                PlotlyFigure(
+                    name='Potential Energy Surface',
+                    data=[
+                        dict(
+                            x=self.parameter_values[0],
+                            y=self.parameter_values[1],
+                            z=self.energy_values,
+                            type='contour',
+                            labels=dict(
+                                x=self.parameter_1_name,
+                                y=self.parameter_2_name,
+                                z='Energy',
+                            ),
+                        ),
+                    ],
+                )
+            )
+        except Exception as e:
+            pass
 
 
 m_package.__init_metainfo__()
