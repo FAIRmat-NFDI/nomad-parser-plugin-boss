297f1f5d4d1c165bd2e7dbf8121e0ffac920b750
ndaelman <ndaelman@physik.hu-berlin.de>
2024-11-22 17:40:26 +0100

    Fix parser

--- src/nomad_parser_plugin_boss/parsers/parser.py
+++ src/nomad_parser_plugin_boss/parsers/parser.py
@@ -3,14 +3,14 @@ from typing import (
 )
 
 if TYPE_CHECKING:
-    from nomad.datamodel.datamodel import (
-        EntryArchive,
-    )
     from structlog.stdlib import (
         BoundLogger,
     )
 
+import numpy as np
+
 from nomad.config import config
+from nomad.datamodel.datamodel import EntryArchive
 from nomad.parsing.parser import MatchingParser
 from nomad.parsing.file_parser.text_parser import TextParser, Quantity as TextQuantity
 
@@ -22,19 +22,18 @@ configuration = config.get_plugin_entry_point(
 
 
 class BossSliceParser(TextParser):
-    def __init__(self):
-        super().__init__(None)
-
     def init_quantities(self):
-        def split_row(full_row: str, key_defs: tuple[str] = ('x_1', 'x_2', 'mu', 'nu')) -> dict[str, float]:
-            return {k: float(x) for k, x in zip(key_defs, full_row.split())}
+        def split_row(full_row: str) -> dict[str, float]:
+            split_row = full_row.split()
+            key_defs = ('x_1', 'x_2', 'mu', 'nu') if len(split_row) == 4 else ('x_1', 'mu', 'nu')
+            return {k: float(x) for k, x in zip(key_defs, split_row)}
 
         # re_float = r'\d\.\d+e[\-\+]\d{2}'
 
-        table_quantities = [
+        self._quantities = [
             TextQuantity(
                 'row',
-                r'((?:\s+\d\.\d+e[\-\+]\d{2}){4})\n',
+                r'((?:\s+\d\.\d+e[\-\+]\d{2}){3,4})\n',
                 str_operation=split_row,
                 repeats=True,
             ),
@@ -54,14 +53,28 @@ class BossPostProcessingParser(MatchingParser):
         slice_parser = BossSliceParser(mainfile=mainfile, logger=logger)
         slice_parser.parse()
 
-        archive.data = [
+        def get_column_unique(column_name: str) -> list[float]:
+            return list({x.get(column_name) for x in slice_parser.results.get('row', [])})
+
+        def get_column(column_name: str) -> list[float]:
+            return [x.get(column_name) for x in slice_parser.results.get('row', [])]
+
+        def reshaping(target: list, dim_1: int, dim_2: int) -> np.ndarray:
+            if dim_2:
+                return np.reshape(target, (dim_1, dim_2))
+            else:
+                return np.reshape(target, (dim_1, -1))
+
+        x_1, x_2 = get_column_unique('x_1'), get_column_unique('x_2')
+
+        archive.m_add_sub_section(EntryArchive.data,
             PotentialEnergySurfaceFit(
                 parameter_1_name='parameter_1_name',
-                parameter_1_values=set(slice_parser.get_quantity('row').data[0]['x_1']),
+                parameter_1_values=x_1,
                 parameter_2_name='parameter_2_name',
-                parameter_2_values=set(slice_parser.get_quantity('row').data[0]['x_2']),
-                energy_values=slice_parser.get_quantity('row').data[0]['mu'],
-                energy_variance=slice_parser.get_quantity('row').data[0]['nu'],
+                parameter_2_values=x_2,
+                energy_values=reshaping(get_column('mu'), len(x_1), len(x_2)),
+                energy_variance=reshaping(get_column('nu'), len(x_1), len(x_2)),
             )
-        ]
+        )
         
